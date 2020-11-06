57d238dc7d90728bd95e2d5fac926ddcb977cc32
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2020-11-06 11:37:48 -0300

    Alteracoes antes de modificar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 
 const App: React.FC = () => (
   <>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+    <StatusBar barStyle="light-content" backgroundColor="#382e2e" />
+    <View style={{ flex: 1, backgroundColor: '#382e2e' }} />
   </>
 );
-
 export default App;
